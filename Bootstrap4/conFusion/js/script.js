d92259eda38800fdf7865536de29e3a67fa6db48
Ahmed E. Haddad <ahmed.e.h.2.55@gmail.com>
2019-10-04 04:46:31 +0300

    #Update npm scripts1

---
+++ Bootstrap4/conFusion/js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+    //Activate  buttons
+    $('#mycarousel').carousel({interval:2000});
+    $("#carouselBtn").click(function(){
+        if($('#carouselBtn').children('span').hasClass('fa-pause')){
+            $('#mycarousel').carousel('pause');
+            $('#carouselBtn').children('span').removeClass('fa-pause');
+            $('#carouselBtn').children('span').addClass('fa-play');
+
+        }
+        else if($('#carouselBtn').children('span').hasClass('fa-play')){
+            $('#mycarousel').carousel('cycle');
+            $('#carouselBtn').children('span').removeClass('fa-play');
+            $('#carouselBtn').children('span').addClass('fa-pause');
+        }
+        
+    });
+   /* $("#carousel-play").click(function(){
+        $("#mycarousel").carousel('cycle');
+    });*/
+});
